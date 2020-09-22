0877b3d26246ef8e4603e99b9eb38ccf650f087b
Vadim Cosman <david.junior1214@gmail.com>
2020-09-22 23:20:14 +0200

    Add files via upload
    Updating the code for scrapping all links to companies page

--- scraping_from_lista_firme.R
+++ scraping_from_lista_firme.R
@@ -17,19 +17,21 @@ for (i in 1:16487) {
 ListOfCompanyUrls = c()
 # the next step is to scrap all the links to every company and save it into another list
 
-#for (i in 1:10) {
-urlOfTheList = ListOfUrls[1]
-theLinks <- urlOfTheList %>%
-  read_html() %>%
-  html_nodes(".clickable-row a") %>%
-  html_attr("href")
-theLinks[1] = ""
-for (i in 1:length(theLinks)){
-  theLinks[i] = paste("https://www.listafirme.ro",theLink[i])
-  theLinks[i] = stri_replace_all_charclass(theLinks[i], "\\p{WHITE_SPACE}", "")
+# Collecting all the links to each company that exists on listafirme
+# Because the site can detect if there is an unusual traffic, you have to change the beginning of the range
+# It takes few minutes until it stops, but, in the mean time it scraps the links to more than 50 000 companies
+for (i in 14604:16487) {
+  urlOfTheList = ListOfUrls[1]
+  theLinks <- urlOfTheList %>%
+    read_html() %>%
+    html_nodes(".clickable-row a") %>%
+    html_attr("href")
+  for (i in 1:length(theLinks)){
+    theLinks[i] = paste("https://www.listafirme.ro",theLinks[i])
+    theLinks[i] = stri_replace_all_charclass(theLinks[i], "\\p{WHITE_SPACE}", "")
+  }
+  ListOfCompanyUrls = c(ListOfCompanyUrls, theLinks)
 }
-ListOfCompanyUrls = c(ListOfCompanyUrls, theLinks)
-#}
 
 
 siteUrl <- "https://www.listafirme.ro/rud-florian-rieger-srl-15721889/"
@@ -38,6 +40,10 @@ tbl.page <- siteUrl %>%
   html_nodes(xpath='//*[@id="bilant"]/table') %>%
   html_table() %>%
   as_tibble(.name_repair = make.names)
+theLinks <- siteUrl %>%
+  read_html() %>%
+  html_nodes(".clickable-row a") %>%
+  html_attr("href")
 
 # Getting just the table from the tibble
 df = tbl.page$X
